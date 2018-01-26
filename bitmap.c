a59dd5c48e6c8dd56030029bdab6baa429b2e284
Aurélien Rainone <aurelien.rainone@gmail.com>
2018-01-26 14:46:45 +0100

    Cosmetics

--- bitmap.c
+++ bitmap.c
@@ -25,7 +25,7 @@ bitmap_hdl bitmap_alloc(size_t nbits)
 {
 	bitmap_t *bm = NULL;
 	if (nbits) {
-		bm = (bitmap_t*) malloc(sizeof(bitmap_t));
+		bm = (bitmap_t *) malloc(sizeof(bitmap_t));
 		bm->nbits = nbits;
 		size_t nbytes = BITS_TO_LONGS(nbits) * sizeof(unsigned long);
 		bm->bits = (unsigned long*) malloc(nbytes);
@@ -35,7 +35,7 @@ bitmap_hdl bitmap_alloc(size_t nbits)
 
 size_t bitmap_length(bitmap_hdl hdl)
 {
-	bitmap_t *bm = (bitmap_t*) hdl;
+	bitmap_t *bm = (bitmap_t *) hdl;
 	if (bm)
 		return bm->nbits;
 	return 0;
@@ -43,7 +43,7 @@ size_t bitmap_length(bitmap_hdl hdl)
 
 void bitmap_free(bitmap_hdl hdl)
 {
-	bitmap_t *bm = (bitmap_t*) hdl;
+	bitmap_t *bm = (bitmap_t *) hdl;
 	if (bm) {
 		free(bm->bits);
 		bm->nbits = 0;
@@ -52,30 +52,30 @@ void bitmap_free(bitmap_hdl hdl)
 
 void bitmap_zero(bitmap_hdl hdl)
 {
-	bitmap_t *bm = (bitmap_t*) hdl;
+	bitmap_t *bm = (bitmap_t *) hdl;
 	unsigned int len = BITS_TO_LONGS(bm->nbits) * sizeof(unsigned long);
 	memset(bm->bits, 0, len);
 }
 
 void bitmap_fill(bitmap_hdl hdl)
 {
-	bitmap_t *bm = (bitmap_t*) hdl;
+	bitmap_t *bm = (bitmap_t *) hdl;
 	unsigned int nlongs = BITS_TO_LONGS(bm->nbits);
 	unsigned int len = (nlongs - 1) * sizeof(unsigned long);
-	memset(bm->bits, 0xff,  len);
+	memset(bm->bits, 0xff, len);
 	bm->bits[nlongs - 1] = BITMAP_LAST_WORD_MASK(bm->nbits);
 }
 
 void bitmap_set(bitmap_hdl hdl, size_t pos)
 {
-	bitmap_t *bm = (bitmap_t*) hdl;
+	bitmap_t *bm = (bitmap_t *) hdl;
 	unsigned long *p = ((unsigned long *) bm->bits) + BIT_WORD(pos);
 	*p |= BIT_MASK(pos);
 }
 
 void bitmap_set_range(bitmap_hdl hdl, size_t pos, int len)
 {
-	bitmap_t *bm = (bitmap_t*) hdl;
+	bitmap_t *bm = (bitmap_t *) hdl;
 	unsigned long *p = (unsigned long *) bm->bits + BIT_WORD(pos);
 	const unsigned int size = pos + len;
 	int bits_to_set = BITS_PER_LONG - (pos % BITS_PER_LONG);
@@ -96,14 +96,14 @@ void bitmap_set_range(bitmap_hdl hdl, size_t pos, int len)
 
 void bitmap_reset(bitmap_hdl hdl, size_t pos)
 {
-	bitmap_t *bm = (bitmap_t*) hdl;
+	bitmap_t *bm = (bitmap_t *) hdl;
 	unsigned long *p = ((unsigned long *)bm->bits) + BIT_WORD(pos);
 	*p &= ~(BIT_MASK(pos));
 }
 
 void bitmap_reset_range(bitmap_hdl hdl, size_t pos, int len)
 {
-	bitmap_t *bm = (bitmap_t*) hdl;
+	bitmap_t *bm = (bitmap_t *) hdl;
 	unsigned long *p = bm->bits + BIT_WORD(pos);
 	const unsigned int size = pos + len;
 	int bits_to_clear = BITS_PER_LONG - (pos % BITS_PER_LONG);
@@ -124,22 +124,22 @@ void bitmap_reset_range(bitmap_hdl hdl, size_t pos, int len)
 
 bool bitmap_get(bitmap_hdl hdl, size_t pos)
 {
-	bitmap_t *bm = (bitmap_t*) hdl;
-	unsigned long *p = ((unsigned long *)bm->bits) + BIT_WORD(pos);
+	bitmap_t *bm = (bitmap_t *) hdl;
+	unsigned long *p = ((unsigned long *) bm->bits) + BIT_WORD(pos);
 	return (*p & BIT_MASK(pos)) != 0;
 }
 
 void bitmap_copy(bitmap_hdl dst, const bitmap_hdl src, size_t nbits)
 {
-	bitmap_t *dstbm = (bitmap_t*) dst;
-	const bitmap_t * const srcbm = (const bitmap_t*) src;
+	bitmap_t *dstbm = (bitmap_t *) dst;
+	const bitmap_t * const srcbm = (const bitmap_t *) src;
 	unsigned int len = BITS_TO_LONGS(nbits) * sizeof(unsigned long);
 	memcpy(dstbm->bits, srcbm->bits, len);
 }
 
 void bitmap_realloc(bitmap_hdl hdl, size_t nbits)
 {
-	bitmap_t *bm = (bitmap_t*) hdl;
+	bitmap_t *bm = (bitmap_t *) hdl;
 	size_t oldnbytes = BITS_TO_LONGS(bm->nbits) * sizeof(unsigned long);
 	size_t newnbytes = BITS_TO_LONGS(nbits) * sizeof(unsigned long);
 	if (nbits != bm->nbits) {
